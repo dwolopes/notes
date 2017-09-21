4cf0d3f33cc556ee24cca0dfb074ea7b41bb685a
Douglas Lopes <douglas.lopes@odonto.cromg.org.br>
2017-09-21 11:22:10 -0300

    Possibilidade de ao clicar num botão pra visualizar outra atualização, fechar a anterior

--- public/js/style.js
+++ public/js/style.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
     $('.btn-info').click(function(){
-    	var id = $(this).val();
-    	//var id = $(this).data();
-    	$.get('requisicao/atualizacao/'+id, function(data){
-    		if(data.success ===	 false) {
-    			$("."+id+">tbody").append('<tr id='+id+'><td>'+data.error_message+'</td></tr>');
-    			$('.'+id).fadeIn(600);
+        var id = $(this).val();
+        //var id = $(this).data();
+        $.get('requisicao/atualizacao/'+id, function(data){
+            if(data.success ===  false) {
+                $("."+id+">tbody").append('<tr id='+id+'><td>'+data.error_message+'</td></tr>');
+                $('.'+id).fadeIn(600);
 
-    		}else {
-    			$("."+id+">tbody").append('<tr id='+id+'><td>'+data.id+'</td><td>'+data.titulo_atualizacao+'</td><td>'
-    			+data.atualizacao+'</td><td>'+data.usuario+'</td></tr>');
-    			$('.'+id).fadeIn(600);
-    		}
-    	});
+            }else {
+                $("."+id+">tbody").append('<tr id='+id+'><td>'+data.id+'</td><td>'+data.titulo_atualizacao+'</td><td>'
+                +data.atualizacao+'</td><td>'+data.usuario+'</td></tr>');
+                $('.'+id).fadeIn(600);
+            }
+        });
     });
 
-    $('.panel-info').mouseleave(function(){
-    	//var id = $(this).val();
-    	$("td").remove();
-    	//var id = $(this).data();
-    	$('.panel-info').fadeOut(800);
+    $('.btn-info').click(function(){
+        //var id = $(this).val();
+        $("td").remove();
+        //var id = $(this).data();
+        $('.panel-info').fadeOut(800);
     });
 });
